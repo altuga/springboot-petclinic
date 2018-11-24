56b68c0c6194b2f81148befed17ce06b32f0725e
altuga@gmail.com <upux1234>
2018-11-24 17:06:05 +0300

    changed owner names

--- src/main/java/com/javaegitimleri/petclinic/dao/mem/OwnerRepositoryInMemoryImpl.java
+++ src/main/java/com/javaegitimleri/petclinic/dao/mem/OwnerRepositoryInMemoryImpl.java
@@ -20,23 +20,23 @@ public class OwnerRepositoryInMemoryImpl implements OwnerRepository {
 	public OwnerRepositoryInMemoryImpl() {
 		Owner owner1 = new Owner();
 		owner1.setId(1L);
-		owner1.setFirstName("Kenan");
-		owner1.setLastName("Sevindik");
+		owner1.setFirstName("James");
+		owner1.setLastName("Gosling");
 
 		Owner owner2 = new Owner();
 		owner2.setId(2L);
-		owner2.setFirstName("Muammer");
-		owner2.setLastName("Yücel");
+		owner2.setFirstName("Davut");
+		owner2.setLastName("Uysal");
 
 		Owner owner3 = new Owner();
 		owner3.setId(3L);
-		owner3.setFirstName("Hümeyra");
-		owner3.setLastName("Sevindik");
+		owner3.setFirstName("Ayşe");
+		owner3.setLastName("Yılmaz");
 		
 		Owner owner4 = new Owner();
 		owner4.setId(4L);
-		owner4.setFirstName("Salim");
-		owner4.setLastName("Sevindik");
+		owner4.setFirstName("Fidan");
+		owner4.setLastName("Kalem");
 
 		ownersMap.put(owner1.getId(), owner1);
 		ownersMap.put(owner2.getId(), owner2);
