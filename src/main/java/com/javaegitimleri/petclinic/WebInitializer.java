515906309bad8e590a29b42bd3b384eadee10ee8
altuga@gmail.com <upux1234>
2018-11-25 12:14:09 +0300

    Initial commit

---
+++ src/main/java/com/javaegitimleri/petclinic/WebInitializer.java
@@ -0,0 +1,11 @@
+package com.javaegitimleri.petclinic;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PetClinicApplication.class);
+    }
+}
